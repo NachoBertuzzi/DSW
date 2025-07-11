3e907994aec0dccd168ba68020c31cdc86b10d9e
JuanRaimundo0 <juanraimundo05@gmail.com>
2025-07-11 11:32:18 -0300

    viernes 11/7 revisado

--- backend/services/entrenadorService.js
+++ backend/services/entrenadorService.js
@@ -2,7 +2,7 @@ const db = require('../db');
 
 exports.getAll = () => {
   return new Promise((resolve, reject) => {
-    db.query('SELECT * FROM entrenadores', (err, results) => {
+    db.query('SELECT * FROM entrenador', (err, results) => {
       if (err) return reject(err);
       resolve(results);
     });
@@ -11,7 +11,7 @@ exports.getAll = () => {
 
 exports.getById = (id) => {
   return new Promise((resolve, reject) => {
-    db.query('SELECT * FROM entrenadores WHERE id = ?', [id], (err, results) => {
+    db.query('SELECT * FROM entrenador WHERE id = ?', [id], (err, results) => {
       if (err) return reject(err);
       resolve(results[0]);
     });
@@ -21,7 +21,7 @@ exports.getById = (id) => {
 exports.create = ({ nombre, especialidad }) => {
   return new Promise((resolve, reject) => {
     db.query(
-      'INSERT INTO entrenadores (nombre, especialidad) VALUES (?, ?)',
+      'INSERT INTO entrenador (nombre, especialidad) VALUES (?, ?)',
       [nombre, especialidad],
       (err, result) => {
         if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.create = ({ nombre, especialidad }) => {
 exports.update = (id, { nombre, especialidad }) => {
   return new Promise((resolve, reject) => {
     db.query(
-      'UPDATE entrenadores SET nombre = ?, especialidad = ? WHERE id = ?',
+      'UPDATE entrenador SET nombre = ?, especialidad = ? WHERE id = ?',
       [nombre, especialidad, id],
       (err, result) => {
         if (err) return reject(err);
@@ -44,9 +44,9 @@ exports.update = (id, { nombre, especialidad }) => {
   });
 };
 
-exports.remove = (id) => {
+exports.delete = (id) => {
   return new Promise((resolve, reject) => {
-    db.query('DELETE FROM entrenadores WHERE id = ?', [id], (err, result) => {
+    db.query('DELETE FROM entrenador WHERE id = ?', [id], (err, result) => {
       if (err) return reject(err);
       resolve(result.affectedRows > 0);
     });

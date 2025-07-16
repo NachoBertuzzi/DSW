ceb0cc8858e011ebae562fa47753a75fde4e21b1
JuanRaimundo0 <juanraimundo05@gmail.com>
2025-07-16 11:57:36 -0300

    elimine models y agregue draft de readme.md del backend

--- backend/models/entrenador.model.js
+++
@@ -1,8 +0,0 @@
-let entrenadores = [];
-let idCounter = 1;
-
-module.exports = {
-    entrenadores,
-    getNextId: () => idCounter++,
-};
-// Este módulo exporta un array de entrenadores y una función para obtener el siguiente ID único.
--- backend/models/localidad.model.js
+++
@@ -1,7 +0,0 @@
-let localidades = [];
-let idCounter = 1;
-
-module.exports = {
-    localidades,
-    getNextId: () => idCounter++
-};
